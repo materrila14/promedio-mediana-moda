261b28403915ee67b6508a54ec6c9d6ffad5bc05
Mateo Quintero Giraldo <materrila14@hotmail.com>
2022-02-22 22:11:45 -0500

    Creo la función para calcular la mediana de cualquier lista

--- mediana.js
+++ mediana.js
@@ -1,41 +1,43 @@
-function calcularMediaAritmetica(lista) {
-    const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento){
-            return valorAcumulado + nuevoElemento;
-        }
-    );
+
+function calcularMediana(lista) {
     
-    const promedioLista = sumaLista / lista.length;
-    
-    return promedioLista
-}
-
-const lista1 = [
-    100, 200, 500, 1000, 500, 400000, 10, 20
-]
-
-lista1.sort((a, b) => a - b)
-
-const mitadLista1 = parseInt(lista1.length / 2);
-
-function esPar(numero) {
-    if (numero % 2 === 0) {
-        return true;
-    } else {
-        return false;
+    function calcularMediaAritmetica(lista) {
+        const sumaLista = lista.reduce(
+            function (valorAcumulado = 0, nuevoElemento){
+                return valorAcumulado + nuevoElemento;
+            }
+        );
+        
+        const promedioLista = sumaLista / lista.length;
+        
+        return promedioLista
     }
-}
 
-let mediana;
+    lista.sort((a, b) => a - b)
+    
+    const mitadlista = parseInt(lista.length / 2);
+    
+    function esPar(numero) {
+        if (numero % 2 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    let mediana;
+    
+    if (esPar(lista.length)) {
+        const elemento1 = lista[mitadlista - 1];
+        const elemento2 = lista[mitadlista];
+        
+        const promedioElementos = calcularMediaAritmetica([elemento1, elemento2]);
+        
+        mediana = promedioElementos;
+        
+    } else {
+        mediana = lista[mitadlista];
+    }
 
-if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitadLista1 - 1];
-    const elemento2 = lista1[mitadLista1];
-
-    const promedioElementos = calcularMediaAritmetica([elemento1, elemento2]);
-
-    mediana = promedioElementos;
-
-} else {
-    mediana = lista1[mitadLista1];
+    return mediana
 }
